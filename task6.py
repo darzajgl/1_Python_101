778c813c2d2ad348cc627b5dbc8f05dd4c3fd4ae
Dariusz Zajglic <darzajgl@gmail.com>
2022-11-20 20:09:18 +0100

    task 6

--- task6.py
+++ task6.py
@@ -27,21 +27,56 @@ def lazy_scribe(sources: list):
     # `result = "pjgyaotvlh2ao2ng"`
     #  
     # Pomóż skrybie zautomatyzowac ten proces! Rezultat przypisz do zmiennej `result`.
-    
+
     # * W liście `sources` znajdują się ciągi znakowe które odpowiadają bilbioteczce skryby.
     # * Ciągi znakowe w 'sources' składają się wyłącznie ze znaków a-z.
     # * Znaki traktuj jako unikalne podmioty, wykorzystanie znaku 'a' z któregoś z ciągów z
     #   z biblioteczki nie oznacza, że nie można wykorzystać znaku 'a' znajdującego się
     #   dalej w tym samym ciągu
 
+    # wszystkie stringi tej samej dlugosci
+    if len(sources) == 0:
+        return ''
+    elif len(sources) == 1:
+        return sources[0]
 
+    string_lengths = [len(s) for s in sources]
+    longest_length: int = max(string_lengths)
+
+    if longest_length == 0:
+        return ''
+
+    sources_with_fixed_length = [s + '' * (longest_length - len(s)) for s in sources]
+
+    # polączenie stringów i usunięcie spacji
+    interweaving_string = ''
+
+    for chars in zip(*sources_with_fixed_length):
+        interweaving_string += ''.join(chars)
+
+    interweaving_string = interweaving_string.replace(' ', '')
+
+    # zebranie wyniku
     result: str = ''
+    current_char = interweaving_string[0]
+    current_counter = 0
 
-    for elements in sources:
-        for letter in elements:
-            str += letter
+    for char in interweaving_string[1:]:
+        if char == current_char:
+            current_counter += 1
+            continue
 
+        if current_counter == 0:
+            result += current_char
+            current_char = char
+        else:
+            result += (str(current_counter + 1) + current_char)
+            current_counter = 0
+            current_char = char
+
+    if current_counter == 0:
+        result += current_char
+    else:
+        result += (str(current_counter + 1) + current_char)
 
     return result
-
-
